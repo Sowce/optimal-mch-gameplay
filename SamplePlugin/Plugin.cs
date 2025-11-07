b7cbf68addab47080f827f9a774a86dd16a53b54
Sowce <lyoxchan@gmail.com>
2025-11-07 20:23:06 +0100

    oomfie

--- SamplePlugin/Plugin.cs
+++ SamplePlugin/Plugin.cs
@@ -1,85 +1,176 @@
-﻿using Dalamud.Game.Command;
+using Dalamud.Game;
+using Dalamud.Game.Command;
+using Dalamud.Hooking;
 using Dalamud.IoC;
 using Dalamud.Plugin;
-using System.IO;
-using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
-using SamplePlugin.Windows;
+using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading;
 
-namespace SamplePlugin;
-
-public sealed class Plugin : IDalamudPlugin
+namespace DrillDoublePress
 {
-    [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
-    [PluginService] internal static ITextureProvider TextureProvider { get; private set; } = null!;
-    [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
-    [PluginService] internal static IClientState ClientState { get; private set; } = null!;
-    [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
-    [PluginService] internal static IPluginLog Log { get; private set; } = null!;
-
-    private const string CommandName = "/pmycommand";
-
-    public Configuration Configuration { get; init; }
-
-    public readonly WindowSystem WindowSystem = new("SamplePlugin");
-    private ConfigWindow ConfigWindow { get; init; }
-    private MainWindow MainWindow { get; init; }
-
-    public Plugin()
+    public class DrillPlugin : IDalamudPlugin
     {
-        Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+        public string Name => "Optimal MCH Gameplay";
 
-        // You might normally want to embed resources and load them from the manifest stream
-        var goatImagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png");
+        private const uint DrillActionId = 16498;
+        private uint backlogCheck = 0;
+        private List<short> pastDrills = new List<short>();
+        private short lastButtonClick = 0;
+        private bool heDoneDidIt = false;
 
-        ConfigWindow = new ConfigWindow(this);
-        MainWindow = new MainWindow(this, goatImagePath);
+        [PluginService] private static IGameInteropProvider GameInteropProvider { get; set; } = null!;
+        [PluginService] private static IPluginLog PluginLog { get; set; } = null!;
+        [PluginService] private static ICommandManager CommandManager { get; set; } = null!;
+        [PluginService] private static IChatGui Chat { get; set; } = null!;
+        [PluginService] private static ISigScanner SigScanner { get; set; } = null!;
+        [PluginService] private static IClientState ClientState { get; set; } = null!;
 
-        WindowSystem.AddWindow(ConfigWindow);
-        WindowSystem.AddWindow(MainWindow);
+        private Hook<GetIconDelegate> GetIconHook;
+        private Hook<IsIconReplaceableDelegate> IsIconReplaceableHook;
+        private Hook<ActionManager.Delegates.UseActionLocation> UseActionLocationHook;
+        private Hook<RaptureHotbarModule.Delegates.ExecuteSlotById> ExecuteSlotByIdHook;
 
-        CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
+        private delegate ulong IsIconReplaceableDelegate(uint actionID);
+        private delegate uint GetIconDelegate(IntPtr actionManager, uint actionID);
+
+        private unsafe bool UseActionLocationDetour(ActionManager* thisPtr, ActionType actionType, uint actionId, ulong targetId, Vector3* location, uint extraParam, byte a7)
         {
-            HelpMessage = "A useful message to display in /xlhelp"
-        });
+            var ret = UseActionLocationHook.Original(thisPtr, actionType, actionId, targetId, location, extraParam, a7);
 
-        // Tell the UI system that we want our windows to be drawn throught he window system
-        PluginInterface.UiBuilder.Draw += WindowSystem.Draw;
+            (new Thread(() =>
+            {
+                Thread.Sleep(500);
+                try
+                {
+                    if (actionId == DrillActionId)
+                    {
+                        PluginLog.Debug($"Drill cast! Action ID: {actionId}, param: {extraParam}");
+                        CheckForDoublePress();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    PluginLog.Error($"Error in UseActionLocationDetour: {ex}");
+                }
+            })).Start();
 
-        // This adds a button to the plugin installer entry of this plugin which allows
-        // toggling the display status of the configuration ui
-        PluginInterface.UiBuilder.OpenConfigUi += ToggleConfigUi;
 
-        // Adds another button doing the same but for the main ui of the plugin
-        PluginInterface.UiBuilder.OpenMainUi += ToggleMainUi;
+            return ret;
+        }
 
-        // Add a simple message to the log with level set to information
-        // Use /xllog to open the log window in-game
-        // Example Output: 00:57:54.959 | INF | [SamplePlugin] ===A cool log message from Sample Plugin===
-        Log.Information($"===A cool log message from {PluginInterface.Manifest.Name}===");
+        private unsafe byte ExecuteSlotByIdDetour(RaptureHotbarModule* thisPtr, uint hotbarId, uint slotId)
+        {
+            var ret = ExecuteSlotByIdHook.Original(thisPtr, hotbarId, slotId);
+
+            lastButtonClick = (short)(hotbarId * 100 + slotId);
+
+            return ret;
+        }
+
+        private void CommandHandler(string command, string arguments)
+        {
+            if (uint.TryParse(arguments, out var value))
+            {
+                if (value == 0)
+                {
+                    Chat.Print($"Wrong value, 1 is lowest allowed Flip");
+                    return;
+                }
+
+                backlogCheck = value - 1;
+                Chat.Print($"New value set to {backlogCheck + 1} PERFECTWORLD");
+            }
+            else
+            {
+                Chat.Print($"Current value is {backlogCheck + 1} PERFECTWORLD");
+            }
+        }
+
+        public DrillPlugin()
+        {
+            CommandManager.AddHandler("/mchisbadvalue", new CommandInfo(this.CommandHandler)
+            {
+                HelpMessage = "Drill backlog size (how many since duplicate to count it, lower is easier)"
+            });
+
+            InitializeHooks();
+        }
+
+        private unsafe void InitializeHooks()
+        {
+            UseActionLocationHook = GameInteropProvider.HookFromAddress<ActionManager.Delegates.UseActionLocation>(
+                ActionManager.MemberFunctionPointers.UseActionLocation,
+                UseActionLocationDetour
+            );
+
+            ExecuteSlotByIdHook = GameInteropProvider.HookFromAddress<RaptureHotbarModule.Delegates.ExecuteSlotById>(
+                RaptureHotbarModule.MemberFunctionPointers.ExecuteSlotById,
+                ExecuteSlotByIdDetour
+            );
+
+            GetIconHook = GameInteropProvider.HookFromAddress<GetIconDelegate>(ActionManager.Addresses.GetAdjustedActionId.Value, GetIconDetour);
+            IsIconReplaceableHook = GameInteropProvider.HookFromAddress<IsIconReplaceableDelegate>(SigScanner.ScanText("40 53 48 83 EC 20 8B D9 48 8B 0D ?? ?? ?? ?? E8 ?? ?? ?? ?? 48 85 C0 74 1F"), IsIconReplaceableDetour);
+
+            UseActionLocationHook.Enable();
+            ExecuteSlotByIdHook.Enable();
+            GetIconHook.Enable();
+            IsIconReplaceableHook.Enable();
+        }
+
+        private ulong IsIconReplaceableDetour(uint actionID) => 1;
+
+        private unsafe uint GetIconDetour(IntPtr actionManager, uint actionID)
+        {
+            try
+            {
+                if (ClientState.LocalPlayer == null)
+                    return GetIconHook.Original(actionManager, actionID);
+
+                if (heDoneDidIt)
+                    return DrillActionId;
+                else
+                    return GetIconHook.Original(actionManager, actionID);
+            }
+            catch (Exception ex)
+            {
+                PluginLog.Error(ex, "Don't crash the game");
+                return GetIconHook.Original(actionManager, actionID);
+            }
+        }
+
+
+        private void CheckForDoublePress()
+        {
+            PluginLog.Debug($"Drill cast from: {lastButtonClick}");
+
+            if (pastDrills.Count != 0 && pastDrills.Count - pastDrills.LastIndexOf(lastButtonClick) >= backlogCheck)
+            {
+                //Process.GetCurrentProcess().Kill();
+                //Chat.Print("BOMBA");
+                heDoneDidIt = true;
+                (new Thread(() =>
+                {
+                    Thread.Sleep(15000);
+                    heDoneDidIt = false;
+                })).Start();
+            }
+
+            pastDrills.Add(lastButtonClick);
+        }
+
+        public void Dispose()
+        {
+            GetIconHook?.Dispose();
+            UseActionLocationHook?.Dispose();
+            UseActionLocationHook?.Dispose();
+            IsIconReplaceableHook?.Dispose();
+
+            CommandManager.RemoveHandler("/mchisbadvalue");
+        }
     }
-
-    public void Dispose()
-    {
-        // Unregister all actions to not leak anythign during disposal of plugin
-        PluginInterface.UiBuilder.Draw -= WindowSystem.Draw;
-        PluginInterface.UiBuilder.OpenConfigUi -= ToggleConfigUi;
-        PluginInterface.UiBuilder.OpenMainUi -= ToggleMainUi;
-        
-        WindowSystem.RemoveAllWindows();
-
-        ConfigWindow.Dispose();
-        MainWindow.Dispose();
-
-        CommandManager.RemoveHandler(CommandName);
-    }
-
-    private void OnCommand(string command, string args)
-    {
-        // In response to the slash command, toggle the display status of our main ui
-        MainWindow.Toggle();
-    }
-    
-    public void ToggleConfigUi() => ConfigWindow.Toggle();
-    public void ToggleMainUi() => MainWindow.Toggle();
 }
